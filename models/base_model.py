60202e232f91b3185aa58b03d6bb7b36ad009cc6
ericmuthoka <muthokaerick@gmail.com>
2023-08-13 04:02:41 -0700

    AirnBnB

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
 
 import uuid
-from datetime import datetime
+import datetime
 
 
 class BaseModel:
-    """BaseModel class that defines common attributes and methods."""
+    """Base class for other models."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize instance attributes."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key,
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+    id = None
+    created_at = None
+    updated_at = None
+
+    def __init__(self, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def __str__(self):
-        """Return the string representation of the instance."""
-        return "[{}] ({}) {}".format(type(self).__name__,
-                             self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-        def save(self):
-            """Update the public instance attribute
-        updated_at with the current datetime."""
-        self.updated_at = datetime.now()
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Return a dictionary
-        containing keys/values of instance attributes."""
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = type(self).__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        """Returns a dictionary with all
+        keys/values of __dict__ of the instance."""
+        dict_ = {k: v for k, v in self.__dict__.items() if v is not None}
+    dict_['__class__'] = self.__class__.__name__
+    dict_['created_at'] = self.created_at.isoformat()
+    dict_['updated_at'] = self.updated_at.isoformat()
+    return dict_
