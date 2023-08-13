067cda080fc3472efc50fe574fd2609e41356c83
ericmuthoka <muthokaerick@gmail.com>
2023-08-13 06:08:33 -0700

    AirnBnB

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,10 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value,
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key,
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
