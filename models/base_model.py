eb0d4faf2192c47f0959859a5ad16969e566a179
ericmuthoka <muthokaerick@gmail.com>
2023-08-13 03:53:46 -0700

    AirnBnB

--- models/base_model.py
+++ models/base_model.py
@@ -14,21 +14,25 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Return the string representation of the instance."""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__,
+                             self.id, self.__dict__)
 
-    def save(self):
-        """Update the public instance attribute updated_at with the current datetime."""
+        def save(self):
+            """Update the public instance attribute
+        updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary containing keys/values of instance attributes."""
+        """Return a dictionary
+        containing keys/values of instance attributes."""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = type(self).__name__
         my_dict['created_at'] = self.created_at.isoformat()
