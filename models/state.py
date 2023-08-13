fbbfcef7c89d8d691ef7f9e1b7b8d18120d8028c
ericmuthoka <muthokaerick@gmail.com>
2023-08-13 07:33:56 -0700

    AirnBnB

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+State Module - Contains State class that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    name = ""
