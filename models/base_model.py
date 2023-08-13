88146036229b75f7b0993ae66dcee27112d30625
ericmuthoka <muthokaerick@gmail.com>
2023-08-13 06:58:01 -0700

    AirnBnB

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 
 import uuid
 from datetime import datetime
-
+from models import storage  # Import the variable storage
 
 class BaseModel:
     """BaseModel class that defines common attributes and methods."""
@@ -13,12 +13,12 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value,
-                                                  '%Y-%m-%dT%H:%M:%S.%f')
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)  # Call new(self) on storage for new instances
 
     def __str__(self):
         """Return the string representation of the instance."""
@@ -28,6 +28,7 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()  # Call save() method of storage
 
     def to_dict(self):
         """Return a dictionary with keys/values of instance attributes."""
