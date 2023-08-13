8bd3e8172f2ec72ff856493c7938f5dbe3fef348
ericmuthoka <muthokaerick@gmail.com>
2023-08-13 04:18:19 -0700

    AirnBnB

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -30,7 +30,8 @@ class TestBaseModel(unittest.TestCase):
         Test the __str__ method of BaseModel.
         """
         instance = BaseModel()
-        expected_str = "[BaseModel] ({}) {}".format(instance.id, instance.__dict__)
+        expected_str = "[BaseModel] ({}) {}".format(instance.id,
+                                                    instance.__dict__)
         self.assertEqual(expected_str, str(instance))
 
     def test_save(self):
@@ -51,8 +52,10 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(instance_dict, dict)
         self.assertEqual(instance_dict['__class__'], 'BaseModel')
         self.assertEqual(instance_dict['id'], instance.id)
-        self.assertEqual(instance_dict['created_at'], instance.created_at.isoformat())
-        self.assertEqual(instance_dict['updated_at'], instance.updated_at.isoformat())
+        self.assertEqual(instance_dict['created_at'],
+                         instance.created_at.isoformat())
+        self.assertEqual(instance_dict['updated_at'],
+                         instance.updated_at.isoformat())
 
 
 if __name__ == '__main__':
